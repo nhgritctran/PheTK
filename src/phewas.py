c93ab5c83c3cac0f82e7ffe4c3cc206a62414a6f
tamctran <123050839+tamctran@users.noreply.github.com>
2023-04-25 23:53:49 -0400

    v0.1

--- src/phewas.py
+++ src/phewas.py
@@ -86,8 +86,8 @@ class PheWAS:
         # exclude_range can be single code (e.g., "777"), single range (e.g., "777-780"),
         # or multiple ranges/codes (e.g., "750-777,586.2")
         phecodes_without_exclude_range = self.merged_df.filter(
-            pl.col("exclude_range").is_null()["phecode"].unique().to_list()
-        )
+            pl.col("exclude_range").is_null()
+        )["phecode"].unique().to_list()
         if phecode in phecodes_without_exclude_range:
             exclude_range = []
         else:
