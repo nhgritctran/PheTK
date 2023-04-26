b73a784e66033f111756be53704618ef3e7d667b
tamctran <123050839+tamctran@users.noreply.github.com>
2023-04-26 16:19:22 -0400

    v0.1

--- src/phewas.py
+++ src/phewas.py
@@ -256,7 +256,7 @@ class PheWAS:
 
         result_dicts = [job.result() for job in tqdm(jobs) if job.result()]
         result_df = pl.from_dicts(result_dicts)
-        result_df = result_df.join(self.phecode_df[["ICD", "flag", "phecode_strings", "phecode_category"]])
+        result_df = result_df.join(self.phecode_df[["ICD", "flag", "phecode_string", "phecode_category"]])
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~    PheWAS Completed    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
