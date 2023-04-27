a875f1660a3a249d2b724834ea6f5dac43960e73
tamctran <123050839+tamctran@users.noreply.github.com>
2023-04-26 20:26:50 -0400

    v0.1

--- src/PheWAS.py
+++ src/PheWAS.py
@@ -226,16 +226,13 @@ class PheWAS:
             var_index = regressors[analysis_covariate_cols].columns.index(self.independent_var_col)
 
             # logistic regression
+            warnings.simplefilter("ignore", ConvergenceWarning)
+            warnings.simplefilter("ignore", LinAlgError)
             y = regressors["y"].to_numpy()
             regressors = regressors[analysis_covariate_cols].to_numpy()
             regressors = sm.tools.add_constant(regressors, prepend=False)
-
-            try:
-                logit = sm.Logit(y, regressors, missing="drop")
-                result = logit.fit(disp=False)
-            except ConvergenceWarning:
-                print(f"Phecode {phecode} ({len(cases)} cases : {len(controls)} controls): Convergence Warning!")
-                print(result.summary())
+            logit = sm.Logit(y, regressors, missing="drop")
+            result = logit.fit(disp=False)
 
             # process result
             base_dict = {"phecode": phecode,
