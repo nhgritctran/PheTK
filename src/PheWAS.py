b828367b302bcd559c19d3864c9d9736214c2543
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-01 16:38:33 -0400

    v0.1

--- src/PheWAS.py
+++ src/PheWAS.py
@@ -290,7 +290,6 @@ class PheWAS:
             with multiprocessing.Pool(multiprocessing.cpu_count()-1) as p:
                 result_dicts = list(tqdm(p.imap(self._logistic_regression, self.phecode_list),
                                                 total=len(self.phecode_list)))
-        print(result_dicts)
         result_dicts = [result for result in result_dicts if result]
         result_df = pl.from_dicts(result_dicts)
         self.result = result_df.join(self.phecode_df[["phecode", "phecode_string", "phecode_category"]].unique(),
