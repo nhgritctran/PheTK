d0dc3bcb1055f7e7899cbcf79bb3c7df6ca4e5fb
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-05 00:36:36 -0400

    v0.1

--- src/plot.py
+++ src/plot.py
@@ -193,9 +193,8 @@ class Manhattan:
                               ha="right",
                               weight="normal",
                               size=12)
-        print(self.color_dict.values())
-        print(adjustText.plt.gca().get_xticklabels())
         for tick_label, tick_color in zip(adjustText.plt.gca().get_xticklabels(), self.color_dict.values()):
+            print(tick_label[2])
             tick_label.set_color(tick_color)
 
         ##########
