b7f9f7b0bf21883088b2b7c2df14951e5cd625f2
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-08 16:52:39 -0400

    v0.1

--- src/PheWAS.py
+++ src/PheWAS.py
@@ -329,7 +329,7 @@ class PheWAS:
             with ThreadPoolExecutor(n_threads) as executor:
                 jobs = [executor.submit(self._logistic_regression, phecode) for phecode in self.phecode_list]
                 # result_dicts = [job.result() for job in tqdm(as_completed(jobs), total=len(self.phecode_list))]
-                result_dicts = [job.result() for job in as_completed(jobs)]
+                result_dicts = [job.result() for job in jobs]
         elif parallelization == "multiprocessing":
             with multiprocessing.Pool(min(n_cores, multiprocessing.cpu_count()-1)) as p:
                 # result_dicts = list(tqdm(p.imap(self._logistic_regression, self.phecode_list),
