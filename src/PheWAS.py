70707ea78a330abddc9211665311a3bcdc782778
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-08 19:30:44 -0400

    v0.1

--- src/PheWAS.py
+++ src/PheWAS.py
@@ -332,6 +332,7 @@ class PheWAS:
         if parallelization == "multithreading":
             with ThreadPoolExecutor(n_threads) as executor:
                 jobs = [executor.submit(self._logistic_regression, phecode) for phecode in self.phecode_list]
+                print(jobs)
                 result_dicts = [job.result() for job in tqdm(as_completed(jobs), total=len(self.phecode_list))]
                 # result_dicts = [job.result() for job in as_completed(jobs)]
         elif parallelization == "multiprocessing":
