437e8de429fb355d69cd32bb4178e2544ed9221d
tamctran <tam.c.tran@outlook.com>
2023-05-16 13:47:17 -0400

    v0.1

--- src/phecode.py
+++ src/phecode.py
@@ -1,6 +1,7 @@
 from google.cloud import bigquery
 import os
 import polars as pl
+import queries
 
 
 def _polars_gbq(query):
@@ -41,72 +42,8 @@ def count_phecode(db="aou", phecode_version="X"):
             return "Invalid phecode version. Please choose either \"1.2\" or \"X\"."
 
         cdr = os.getenv("WORKSPACE_CDR")
-        icd_query = f"""
-        SELECT DISTINCT
-            *
-        FROM
-            (
-                (
-                SELECT DISTINCT
-                    CAST(co.person_id AS STRING) AS person_id,
-                    co.condition_start_date AS date,
-                    c.concept_code AS ICD
-                FROM
-                    {cdr}.condition_occurrence AS co
-                INNER JOIN
-                    {cdr}.concept AS c
-                ON
-                    co.condition_source_value = c.concept_code
-                WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
-                )
-            UNION DISTINCT
-                (
-                SELECT DISTINCT
-                    CAST(co.person_id AS STRING) AS person_id,
-                    co.condition_start_date AS date,
-                    c.concept_code AS ICD
-                FROM
-                    {cdr}.condition_occurrence AS co
-                INNER JOIN
-                    {cdr}.concept AS c
-                ON
-                    co.condition_source_concept_id = c.concept_id
-                WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
-                )
-            UNION DISTINCT
-                (
-                SELECT DISTINCT
-                    CAST(o.person_id AS STRING) AS person_id,
-                    o.observation_date AS date,
-                    c.concept_code AS ICD
-                FROM
-                    {cdr}.observation AS o
-                INNER JOIN
-                    {cdr}.concept as c
-                ON
-                    o.observation_source_value = c.concept_code
-                WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
-                )
-            UNION DISTINCT
-                (
-                SELECT DISTINCT
-                    CAST(o.person_id AS STRING) AS person_id,
-                    o.observation_date AS date,
-                    c.concept_code AS ICD
-                FROM
-                    {cdr}.observation AS o
-                INNER JOIN
-                    {cdr}.concept as c
-                ON
-                    o.observation_source_concept_id = c.concept_id
-                WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
-                )
-            )
-        """
+        icd_query = queries.phecode_icd_query
+
         print("Start querying ICD codes...")
         icd_events = _polars_gbq(icd_query)
         print("Mapping ICD codes to phecodes...")
---
+++ src/queries.py
@@ -0,0 +1,133 @@
+phecode_icd_query = f"""
+        SELECT DISTINCT
+            *
+        FROM
+            (
+                (
+                SELECT DISTINCT
+                    CAST(co.person_id AS STRING) AS person_id,
+                    co.condition_start_date AS date,
+                    c.concept_code AS ICD
+                FROM
+                    {cdr}.condition_occurrence AS co
+                INNER JOIN
+                    {cdr}.concept AS c
+                ON
+                    co.condition_source_value = c.concept_code
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
+                )
+            UNION DISTINCT
+                (
+                SELECT DISTINCT
+                    CAST(co.person_id AS STRING) AS person_id,
+                    co.condition_start_date AS date,
+                    c.concept_code AS ICD
+                FROM
+                    {cdr}.condition_occurrence AS co
+                INNER JOIN
+                    {cdr}.concept AS c
+                ON
+                    co.condition_source_concept_id = c.concept_id
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
+                )
+            UNION DISTINCT
+                (
+                SELECT DISTINCT
+                    CAST(o.person_id AS STRING) AS person_id,
+                    o.observation_date AS date,
+                    c.concept_code AS ICD
+                FROM
+                    {cdr}.observation AS o
+                INNER JOIN
+                    {cdr}.concept as c
+                ON
+                    o.observation_source_value = c.concept_code
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
+                )
+            UNION DISTINCT
+                (
+                SELECT DISTINCT
+                    CAST(o.person_id AS STRING) AS person_id,
+                    o.observation_date AS date,
+                    c.concept_code AS ICD
+                FROM
+                    {cdr}.observation AS o
+                INNER JOIN
+                    {cdr}.concept as c
+                ON
+                    o.observation_source_concept_id = c.concept_id
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM")
+                )
+            )
+        """
+
+ehr_code_query = f"""
+        SELECT DISTINCT
+            *
+        FROM
+            (
+                (
+                SELECT DISTINCT
+                    CAST(co.person_id AS STRING) AS person_id,
+                    co.condition_start_date AS date,
+                    c.concept_code AS code
+                FROM
+                    {cdr}.condition_occurrence AS co
+                INNER JOIN
+                    {cdr}.concept AS c
+                ON
+                    co.condition_source_value = c.concept_code
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                )
+            UNION DISTINCT
+                (
+                SELECT DISTINCT
+                    CAST(co.person_id AS STRING) AS person_id,
+                    co.condition_start_date AS date,
+                    c.concept_code AS code
+                FROM
+                    {cdr}.condition_occurrence AS co
+                INNER JOIN
+                    {cdr}.concept AS c
+                ON
+                    co.condition_source_concept_id = c.concept_id
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                )
+            UNION DISTINCT
+                (
+                SELECT DISTINCT
+                    CAST(o.person_id AS STRING) AS person_id,
+                    o.observation_date AS date,
+                    c.concept_code AS code
+                FROM
+                    {cdr}.observation AS o
+                INNER JOIN
+                    {cdr}.concept as c
+                ON
+                    o.observation_source_value = c.concept_code
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                )
+            UNION DISTINCT
+                (
+                SELECT DISTINCT
+                    CAST(o.person_id AS STRING) AS person_id,
+                    o.observation_date AS date,
+                    c.concept_code AS code
+                FROM
+                    {cdr}.observation AS o
+                INNER JOIN
+                    {cdr}.concept as c
+                ON
+                    o.observation_source_concept_id = c.concept_id
+                WHERE
+                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                )
+            )
+        """
