86e8bf5e9c7c144d3a12420f2ef099acf55c6af1
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-17 00:03:39 -0400

    v0.1

--- src/covariate.py
+++ src/covariate.py
@@ -4,31 +4,52 @@ import queries
 import utils
 
 
-def get_covariate(participant_ids,
-                  natural_age=True,
-                  age_at_last_event=True,
-                  sex_at_birth=True,
-                  ehr_length=True,
-                  dx_code_count=True,
-                  genetic_ancestry=False,
-                  pc_count=0,
-                  cdr_version=7):
+def get_covariates(participant_ids,
+                   natural_age=True,
+                   age_at_last_event=True,
+                   sex_at_birth=True,
+                   ehr_length=True,
+                   dx_code_count=True,
+                   genetic_ancestry=False,
+                   pc_count=0,
+                   cdr_version=7):
 
-    if is_instance(participant_ids, str):
-        participant_ids = [participant_ids]
+    # initial data prep
+    if isinstance(participant_ids, str):
+        participant_ids = (participant_ids, )
+    elif isinstance(participant_ids, list):
+        participant_ids = tuple(participant_ids)
+    df = pl.DataFrame(participant_ids, schema={"person_id": str})
 
+    # All of Us CDR v7
     if cdr_version == 7:
         cdr = os.getenv("WORKSPACE_CDR")
+
         if natural_age:
-            natural_age_df = utils.polars_gbq(queries.natural_age_query(cdr))
-        if age_at_last_event:
-            pass
+            natural_age_df = utils.polars_gbq(queries.natural_age_query(cdr, participant_ids))
+            df = df.join(natural_age_df, how="left", on="person_id")
+
+        if age_at_last_event or ehr_length or dx_code_count:
+            temp_df = utils.polars_gbq(queries.ehr_dx_code_query(cdr, participant_ids))
+            cols_to_keep = []
+            if age_at_last_event:
+                cols_to_keep.append("age_at_last_event")
+            if ehr_length:
+                cols_to_keep.append("ehr_length")
+            if dx_code_count:
+                cols_to_keep.append("dx_code_count")
+            df = df.join(temp_df[cols_to_keep], how="left", on="person_id")
+
         if sex_at_birth:
-            pass
+            sex_df = utils.polars_gbq(queries.sex_at_birth(cdr, participant_ids))
+            df = df.join(sex_df, how="left", on="person_id")
+
         if genetic_ancestry:
             pass
         if pc_count > 0:
             pass
+
+        return df
 
     else:
         print("Currently, only All of Us CDR v7 is supported.")
--- src/queries.py
+++ src/queries.py
@@ -69,21 +69,52 @@ def phecode_icd_query(cdr):
                     c.vocabulary_id in ("ICD9CM", "ICD10CM")
                 )
             )
-        """
+    """
 
     return query
 
 
-def ehr_dx_code_query(cdr):
+def natural_age_query(cdr, participant_ids):
     """
     this method is exclusively for All of Us platform
     :param cdr: All of Us Curated Data Repository
+    :param participant_ids: list of participant IDs to query
     :return: a SQL query that would generate a table
-             contains participant IDs and their ICD/SNOMED codes from unique dates
+            contains participant IDs and their natural age
+    """
+
+    if isinstance(participant_ids, str):
+        participant_ids = (participant_ids, )
+    elif isinstance(participant_ids, list):
+        participant_ids = tuple(participant_ids)
+
+    query: str = f"""
+        SELECT
+            person_id, 
+            DATETIME_DIFF(CURRENT_DATETIME(), DATETIME(birth_datetime), DAY)/365.2425 AS natural_age
+        FROM
+            {cdr}.person
+        WHERE
+            person_id IN {participant_ids}
+    """
+
+    return query
+
+
+def ehr_dx_code_query(cdr, participant_ids):
+    """
+    this method is exclusively for All of Us platform
+    :param cdr: All of Us Curated Data Repository
+    :param participant_ids: list of participant IDs to query
+    :return: a SQL query that would generate a table contains participant IDs and
+            their ehr length (days), diagnosis code count(ICD & SNOMED), and age at last event
     """
     query: str = f"""
         SELECT DISTINCT
-            *
+            df1.person_id,
+            DATETIME_DIFF(MAX(date), MIN(date), DAY) AS ehr_length,
+            COUNT(code) AS dx_code_count,
+            DATETIME_DIFF(MAX(date), MIN(birthday), DAY)/365.2425 AS age_at_last_event,
         FROM
             (
                 (
@@ -98,7 +129,9 @@ def ehr_dx_code_query(cdr):
                 ON
                     co.condition_source_value = c.concept_code
                 WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                    c.vocabulary_id IN ("ICD9CM", "ICD10CM", "SNOMED")
+                    AND
+                    person_id IN {participant_ids}
                 )
             UNION DISTINCT
                 (
@@ -113,7 +146,9 @@ def ehr_dx_code_query(cdr):
                 ON
                     co.condition_source_concept_id = c.concept_id
                 WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                    c.vocabulary_id IN ("ICD9CM", "ICD10CM", "SNOMED")
+                    AND
+                    person_id IN {participant_ids}
                 )
             UNION DISTINCT
                 (
@@ -124,11 +159,13 @@ def ehr_dx_code_query(cdr):
                 FROM
                     {cdr}.observation AS o
                 INNER JOIN
-                    {cdr}.concept as c
+                    {cdr}.concept AS c
                 ON
                     o.observation_source_value = c.concept_code
                 WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                    c.vocabulary_id IN ("ICD9CM", "ICD10CM", "SNOMED")
+                    AND
+                    person_id IN {participant_ids}
                 )
             UNION DISTINCT
                 (
@@ -139,33 +176,87 @@ def ehr_dx_code_query(cdr):
                 FROM
                     {cdr}.observation AS o
                 INNER JOIN
-                    {cdr}.concept as c
+                    {cdr}.concept AS c
                 ON
                     o.observation_source_concept_id = c.concept_id
                 WHERE
-                    c.vocabulary_id in ("ICD9CM", "ICD10CM", "SNOMED")
+                    c.vocabulary_id IN ("ICD9CM", "ICD10CM", "SNOMED")
+                    AND
+                    person_id IN {participant_ids}
                 )
-            )
-        """
+            ) AS df1
+        INNER JOIN
+            (
+                SELECT
+                    CAST(person_id as STRING) AS person_id, 
+                    EXTRACT(DATE FROM DATETIME(birth_datetime)) AS birthday
+                FROM
+                    {cdr}.person
+                WHERE
+                    person_id IN {participant_ids}
+            ) AS df2
+        ON
+            df1.person_id = df2.person_id
+        GROUP BY 
+            df1.person_id
+    """
 
     return query
 
 
-def natural_age_query(cdr, participant_ids=None):
-    if participant_ids:
-        if isinstance(participant_ids, str):
-            participant_ids = (participant_ids, )
-        if isinstance(participant_ids, list):
-            participant_ids = tuple(participant_ids)
-
-        query: str = f"""
+def sex_at_birth(cdr, participant_ids):
+    """
+    this method is exclusively for All of Us platform
+    :param cdr: All of Us Curated Data Repository
+    :param participant_ids: list of participant IDs to query
+    :return: a SQL query that would generate a table contains participant IDs and
+            their sex at birth; male = 1, female = 0
+    """
+    query: str = f"""
         SELECT
-            person_id, 
-            DATETIME_DIFF(CURRENT_DATETIME(), DATETIME(birth_datetime), DAY)/365.2425 AS natural_age
+            *
         FROM
-            {cdr}.person
-        WHERE
-            person_id IN {participant_ids}
-        """
+            (
+                (
+                SELECT
+                    person_id,
+                    1 AS sex_at_birth
+                FROM
+                    {cdr}.person
+                WHERE
+                    sex_at_birth_source_concept_id = 1585846
+                )
+            UNION DISTINCT
+                (
+                SELECT
+                    person_id,
+                    0 AS sex_at_birth
+                FROM
+                    {cdr}.person
+                WHERE
+                    sex_at_birth_source_concept_id = 1585847
+                )
+            UNION DISTINCT
+                (
+                SELECT
+                    person_id,
+                    1 AS sex_at_birth
+                FROM
+                    {cdr}.observation
+                WHERE
+                    observation_source_concept_id = 8507
+                )
+            UNION DISTINCT
+                (
+                SELECT
+                    person_id,
+                    0 AS sex_at_birth
+                FROM
+                    {cdr}.observation
+                WHERE
+                    observation_source_concept_id = 8532
+                )
+            )
+    """
 
-        return query
+    return query
