826d944f45ddf67a2a0d4033d75e1a0db85d021a
tamctran <tam.c.tran@outlook.com>
2023-05-16 20:35:17 -0400

    v0.1

--- src/covariate.py
+++ src/covariate.py
@@ -4,18 +4,32 @@ import queries
 import utils
 
 
-def get_covariate(natural_age=False,
-                  age_at_last_event=False,
-                  sex_at_birth=False,
+def get_covariate(participant_ids,
+                  natural_age=True,
+                  age_at_last_event=True,
+                  sex_at_birth=True,
+                  ehr_length=True,
+                  dx_code_count=True,
                   genetic_ancestry=False,
-                  pc_count=0):
-    if natural_age:
-        pass
-    if age_at_last_event:
-        pass
-    if sex_at_birth:
-        pass
-    if genetic_ancestry:
-        pass
-    if pc_count > 0:
-        pass
+                  pc_count=0,
+                  cdr_version=7):
+
+    if is_instance(participant_ids, str):
+        participant_ids = [participant_ids]
+
+    if cdr_version == 7:
+        cdr = os.getenv("WORKSPACE_CDR")
+        if natural_age:
+            natural_age_df = utils.polars_gbq(queries.natural_age_query(cdr))
+        if age_at_last_event:
+            pass
+        if sex_at_birth:
+            pass
+        if genetic_ancestry:
+            pass
+        if pc_count > 0:
+            pass
+
+    else:
+        print("Currently, only All of Us CDR v7 is supported.")
+        return
--- src/queries.py
+++ src/queries.py
@@ -74,7 +74,7 @@ def phecode_icd_query(cdr):
     return query
 
 
-def ehr_code_query(cdr):
+def ehr_dx_code_query(cdr):
     """
     this method is exclusively for All of Us platform
     :param cdr: All of Us Curated Data Repository
@@ -149,3 +149,23 @@ def ehr_code_query(cdr):
         """
 
     return query
+
+
+def natural_age_query(cdr, participant_ids=None):
+    if participant_ids:
+        if isinstance(participant_ids, str):
+            participant_ids = (participant_ids, )
+        if isinstance(participant_ids, list):
+            participant_ids = tuple(participant_ids)
+
+        query: str = f"""
+        SELECT
+            person_id, 
+            DATETIME_DIFF(CURRENT_DATETIME(), DATETIME(birth_datetime), DAY)/365.2425 AS natural_age
+        FROM
+            {cdr}.person
+        WHERE
+            person_id IN {participant_ids}
+        """
+
+        return query
