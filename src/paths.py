5b087d6cd8e230b7aa54f8d0654d56ef3ad91f6e
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-17 23:58:31 -0400

    v0.1

--- src/paths.py
+++ src/paths.py
@@ -1,2 +1,5 @@
 cdr6_ancestry_pred_path = "gs://fc-aou-datasets-controlled/v6/wgs/vcf/aux/ancestry/ancestry_preds.tsv"
 cdr7_ancestry_pred_path = "gs://fc-aou-datasets-controlled/v7/wgs/short_read/snpindel/aux/ancestry/ancestry_preds.tsv"
+
+cdr7_mt_path = "gs://fc-aou-datasets-controlled/v7/wgs/short_read/snpindel/acaf_threshold/multiMT/hail.mt"
+
