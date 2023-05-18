521164100e3abfce07fcb63859ee169e5fba57fa
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-18 01:15:23 -0400

    v0.1

--- src/covariate.py
+++ src/covariate.py
@@ -121,7 +121,7 @@ def get_covariates(participant_ids,
 
     result_list = [result for result in result_list if result is not None]
     df = result_list[0]
-    for i in range(1, len(chunks) + 1):
+    for i in range(1, len(chunks)):
         df = pl.concat([df, result_list[i]])
     df = df.unique()
 
