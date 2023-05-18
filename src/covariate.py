5b700f3e9996bd6fbd535e304f8ff12766780618
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-18 00:48:06 -0400

    v0.1

--- src/covariate.py
+++ src/covariate.py
@@ -101,7 +101,7 @@ def get_covariates(participant_ids,
                    genetic_ancestry=False,
                    first_n_pcs=0,
                    cdr_version=7):
-    chunks = [list(participant_ids)[i*100000:(i+1)*100000] for i in range(len(participant_ids) % 100000)]
+    chunks = [list(participant_ids)[i*1000:(i+1)*1000] for i in range(len(participant_ids) % 1000)]
     with ThreadPoolExecutor() as executor:
         jobs = [
             executor.submit(
