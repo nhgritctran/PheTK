af6dce23acea6a05d8729a2c83c91a836597f0ee
tamctran <123050839+tamctran@users.noreply.github.com>
2023-05-18 01:09:20 -0400

    v0.1

--- src/covariate.py
+++ src/covariate.py
@@ -119,6 +119,7 @@ def get_covariates(participant_ids,
         ]
         result_list = [job.result() for job in tqdm(as_completed(jobs), total=len(chunks))]
 
+    result_list = [result for result in result_list if result is not None]
     df = result_list[0]
     for i in range(1, len(chunks) + 1):
         df = pl.concat([df, result_list[i]])
