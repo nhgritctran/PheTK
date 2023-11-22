59912503456b56fbedb0f98e74f81c6767cd1247
nhgritctran <123414601+nhgritctran@users.noreply.github.com>
2023-11-22 01:04:31 -0500

    v0.1

--- src/Phecode.py
+++ src/Phecode.py
@@ -5,18 +5,34 @@ import sys
 
 
 class Phecode:
+    """
+    Class Phecode extract ICD codes and map ICD codes to phecodes version 1.2 and X.
+    Currently, supports ICD code extraction for All of Us OMOP data.
+    For other databases, user is expected to provide an ICD code table for all participants in cohort of interest.
+    """
 
-    def __init__(self, db="aou"):
+    def __init__(self, db="aou", icd_df_path=None):
+        """
+        Instantiate based on parameter db
+        :param db: supports:
+            "aou": All of Us OMOP database
+            "custom": other databases; icd_df must be not None if db = "custom"
+        :param icd_df_path: path to ICD table csv file; required columns are "person_id", "ICD", and "vocabulary_id";
+            "vocabulary_id" values should be "ICD9CM" or "ICD10CM"
+        """
         if db == "aou":
             self.db = db
+            self.cdr = os.getenv("WORKSPACE_CDR")
+            self.icd_query = _queries.phecode_icd_query(self.cdr)
+            print("\033[1mStart querying ICD codes...")
+            self.icd_events = _utils.polars_gbq(self.icd_query)
+            print("\033[1mDone!")
+        elif db == "custom":
+            self.icd_events = pl.read_csv(icd_df_path,
+                                          dtypes={"ICD": str})
         else:
-            print("Invalid database. Currently, only \"aou\" (All of Us) is supported.")
+            print("Invalid database. Parameter db only accepts \"aou\" (All of Us) or \"custom\".")
             sys.exit(0)
-        self.cdr = os.getenv("WORKSPACE_CDR")
-        self.icd_query = _queries.phecode_icd_query(self.cdr)
-        print("\033[1mStart querying ICD codes...")
-        self.icd_events = _utils.polars_gbq(self.icd_query)
-        print("\033[1mDone!")
 
     def count_phecode(self, phecode_version="X"):
         """
@@ -43,24 +59,31 @@ class Phecode:
 
         # make a copy of self.icd_events
         icd_events = self.icd_events.clone()
+        icd_events = icd_events.with_columns(pl.when(pl.col("vocabulary_id") == "ICD9CM")
+                                             .then(9)
+                                             .otherwise(10)
+                                             .alias("flag"))
         icd_events = icd_events.drop(["date", "vocabulary_id"])
 
         print()
         print(f"\033[1mMapping ICD codes to phecode {phecode_version}...")
         if phecode_version == "X":
-            phecode_counts = icd_events.join(phecode_df[["phecode", "ICD"]], how="inner", on="ICD")
+            phecode_counts = icd_events.join(phecode_df[["phecode", "ICD"]], how="inner", on=["ICD", "flag"])
         elif phecode_version == "1.2":
-            phecode_counts = icd_events.join(phecode_df[["phecode_unrolled", "ICD"]], how="inner", on="ICD")
+            phecode_counts = icd_events.join(phecode_df[["phecode_unrolled", "ICD"]], how="inner", on=["ICD", "flag"])
             phecode_counts = phecode_counts.rename({"phecode_unrolled": "phecode"})
         else:
             phecode_counts = None
         if not phecode_counts.is_empty() or phecode_counts is not None:
             phecode_counts = phecode_counts.groupby(["person_id", "phecode"]).count()
+        phecode_counts = phecode_counts.drop(["flag"])
 
         # report result
         if not phecode_counts.is_empty() or phecode_counts is not None:
             if self.db == "aou":
                 db_val = "All of Us"
+            elif self.db == "custom":
+                db_val = "custom"
             else:
                 db_val = None
             file_name = self.db + "_phecode" + phecode_version.upper().replace(".", "") + "_counts.csv"
