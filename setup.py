842db00bd7bdec3694a0eeed8b956a020207fd80
nhgritctran <123414601+nhgritctran@users.noreply.github.com>
2023-12-17 23:18:33 -0500

    v0.1

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PheTK - Phenotype Toolkit",
+    name="PheTK",
     version="0.1.0",
+    author="Tran, Tam",
+    description="PheTK - Phenotype Toolkit",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nhgritctran/PheTK",
     packages=find_packages(where="src"),
+    package_dir={"": "src"},
     package_data={
         "PheTK": ["phecode/*.csv"]
-    }
+    },
+    python_requires=">=3.7"
 )
