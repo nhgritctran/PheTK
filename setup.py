4efbcc947882f4084f3f3ff87bad54e697e87d9d
nhgritctran <123414601+nhgritctran@users.noreply.github.com>
2023-12-17 23:55:39 -0500

    v0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="PheTK",
-    version="0.1.0",
+    version="0.1.1",
     author="Tran, Tam",
     description="PheTK - Phenotype Toolkit",
     long_description=open("README.md").read(),
