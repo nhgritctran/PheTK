e9bc02376b0dfada0fb89fa3e65572a8bd58b839
nhgritctran <123414601+nhgritctran@users.noreply.github.com>
2024-02-14 08:43:05 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PheTK",
-    version="0.1.30",
+    version="0.1.32",
     author="Tran, Tam",
     description="PheTK - Phenotype Toolkit",
     long_description=open("README.md").read(),
