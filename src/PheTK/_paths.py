13cdb17488dc7a89fe129d4c9e5f680a36c83080
nhgritctran <123414601+nhgritctran@users.noreply.github.com>
2025-01-31 18:46:59 -0500

    v0.1.46rc1 - update to prepare for AoU v8 data

--- src/PheTK/_paths.py
+++ src/PheTK/_paths.py
@@ -1,8 +1,10 @@
 cdr6_ancestry_pred_path = "gs://fc-aou-datasets-controlled/v6/wgs/vcf/aux/ancestry/ancestry_preds.tsv"
 cdr7_ancestry_pred_path = "gs://fc-aou-datasets-controlled/v7/wgs/short_read/snpindel/aux/ancestry/ancestry_preds.tsv"
+cdr8_ancestry_pred_path = "gs://fc-aou-datasets-controlled/v8/wgs/short_read/snpindel/aux/ancestry/ancestry_preds.tsv"
 
 cdr6_mt_path = "gs://fc-aou-datasets-controlled/v6/wgs/hail.mt"
 cdr7_mt_path = "gs://fc-aou-datasets-controlled/v7/wgs/short_read/snpindel/acaf_threshold_v7.1/splitMT/hail.mt"
+cdr8_mt_path = "gs://fc-aou-datasets-controlled/v8/wgs/short_read/snpindel/acaf_threshold/splitMT/hail.mt"
 
 # obsolete paths
 # cdr7_mt_path = "gs://fc-aou-datasets-controlled/v7/wgs/short_read/snpindel/acaf_threshold_v7.1/multiMT/hail.mt"
